d6f1a4e3cf04b614d7101db37fc3f9fc7e3ce48c
alifmaulidanar <alifmaulidanr@gmail.com>
2024-12-27 23:42:10 +0700

    Updated radar-backup

--- src/utils/radar-backup.ts
+++ src/utils/radar-backup.ts
@@ -1,10 +1,9 @@
 import Radar from 'react-native-radar';
-import supabase from './supabase';
-import { getUserData } from '../api/auth';
 
+// Initialize Radar SDK
 const initializeRadar = (publishableKey: string) => {
-  // Initialize Radar SDK with your publishable key
   Radar.initialize(publishableKey);
+  console.log('Radar successfully initialized.');
 };
 
 // Request permissions for foreground and background location
@@ -19,94 +18,177 @@ const requestLocationPermissions = async () => {
 
 // Track location once (Foreground tracking)
 const trackLocationOnce = async () => {
-  // Radar.trackOnce().then((result) => {
-  //   console.log('Location:', result.location);
-  //   console.log('Events:', result.events);
-  //   console.log('User Metadata:', result.user);
-  // }).catch((err) => {
-  //   console.error('Error tracking location:', err);
-  // });
-  try {
-    const result = await Radar.trackOnce();
+  Radar.trackOnce().then((result) => {
     console.log('Location:', result.location);
     console.log('Events:', result.events);
     console.log('User Metadata:', result.user);
-  } catch (err) {
+  }).catch((err) => {
     console.error('Error tracking location:', err);
-  }
+  });
 };
 
 // Start background location tracking with Radar presets
-const startBackgroundTracking = (preset: 'EFFICIENT' | 'RESPONSIVE' | 'CONTINUOUS') => {
-  console.log('Starting background tracking with preset:', preset);
-  switch (preset) {
-    case 'EFFICIENT':
-      Radar.startTrackingEfficient();
-      break;
-    case 'RESPONSIVE':
-      Radar.startTrackingResponsive();
-      break;
-    case 'CONTINUOUS':
-      Radar.startTrackingContinuous();
-      break;
-    default:
-      console.error('Invalid preset for background tracking.');
+const startBackgroundTracking = async (preset: 'EFFICIENT' | 'RESPONSIVE' | 'CONTINUOUS') => {
+  // Request location permissions
+  await requestLocationPermissions();
+
+  // Set default trip options
+  const defaultTripOptions: any = {
+    externalId: '0193e9e3-7243-7610-8566-119a522a6b79',
+    destinationGeofenceTag: 'jalan-raya',
+    destinationGeofenceExternalId: '0193e9e3-1ae3-7efc-b981-e4c3311c7f85',
+    mode: 'bike',
+    approachingThreshold: 1,
   }
+
+  // Set default tracking options
+  const defaultTrackingOptions: any = {
+    desiredStoppedUpdateInterval: 30,
+    fastestStoppedUpdateInterval: 30,
+    desiredMovingUpdateInterval: 30,
+    fastestMovingUpdateInterval: 30,
+    desiredSyncInterval: 20,
+    desiredAccuracy: "high",
+    stopDuration: 0,
+    stopDistance: 0,
+    startTrackingAfter: null,
+    stopTrackingAfter: null,
+    replay: "all",
+    sync: "all",
+    useStoppedGeofence: false,
+    stoppedGeofenceRadius: 0,
+    useMovingGeofence: false,
+    movingGeofenceRadius: 0,
+    syncGeofences: false,
+    syncGeofencesLimit: 0,
+    foregroundServiceEnabled: true,
+    beacons: false,
+  }
+
+  // Start trip
+  Radar.startTrip({
+    tripOptions: defaultTripOptions,
+    trackingOptions: defaultTrackingOptions
+  }).then((result) => {
+    console.log('Trip started:', result);
+  });
+  console.log('Trip started...');
+
+  // Track location once in foreground
+  trackLocationOnce();
+  const now = new Date().toLocaleString("id-ID", { timeZone: "Asia/Jakarta" })
+  console.log('Location tracked once in foreground:', now);
+
+  // console.log('Starting background tracking with preset:', preset);
+  // switch (preset) {
+  //   case 'EFFICIENT':
+  //     Radar.startTrackingEfficient();
+  //     break;
+  //   case 'RESPONSIVE':
+  //     Radar.startTrackingResponsive();
+  //     break;
+  //   case 'CONTINUOUS':
+  //     Radar.startTrackingContinuous();
+  //     break;
+  //   default:
+  //     console.error('Invalid preset for background tracking.');
+  // }
+
+  // // Start custom background tracking
+  // Radar.mockTracking({
+  //   origin: { // pondok kopi arabika viii blok ac 6 no. 5
+  //     latitude: -6.2324146301045875,
+  //     longitude: 106.94557809708698,
+  //   },
+  //   destination: { // farmers pondok kopi
+  //     latitude: -6.226135796446481,
+  //     longitude: 106.94324735176554,
+  //   },
+  //   mode: 'bike', // motor
+  //   steps: 1, // 1 location update
+  //   interval: 30, // per 30 seconds
+  // })
+  // console.log('Mock tracking started...');
+
+
+  // Update trip
+  Radar.updateTrip({
+    status: 'started',
+    options: {
+      externalId: '0193e9e3-7243-7610-8566-119a522a6b79',
+      mode: 'bike',
+      destinationGeofenceTag: 'jalan-raya',
+      destinationGeofenceExternalId: '0193e9e3-1ae3-7efc-b981-e4c3311c7f85',
+      approachingThreshold: 1,
+    },
+  }).then((result) => {
+    console.log('Trip updated:', result);
+  });
+  console.log('Trip updated...');
+
+  // Set foreground service options
+  // Radar.setForegroundServiceOptions({
+  //   options: {
+  //     text: "Pekerjaan dimulai.",
+  //     title: "Lokasi diperbarui.",
+  //     updatesOnly: false,
+  //     importance: 2,
+  //     activity: 'com.alifmaulidanar.pastitracking'
+  //   }
+  // });
+
+  // Check if background tracking is active
+  const isTracking = Radar.isTracking();
+  console.log('Background tracking status:', isTracking);
+
+  // Listen for location updates
+  listenForLocationUpdates();
 };
 
 // Stop background location tracking
 const stopBackgroundTracking = () => {
+  // Complete trip
+  Radar.completeTrip().then((result) => {
+    console.log('Trip completed:', result);
+  });
+  console.log('Trip completed...');
+
+  // Stop background tracking
+  console.log('Stopping background tracking...');
+  Radar.stopTracking();
+};
+
+// Cancel trip
+const cancelTrip = () => {
+  Radar.cancelTrip().then((result) => {
+    console.log('Trip canceled:', result);
+  });
+  console.log('Trip canceled...');
+
+  // Stop background tracking
   console.log('Stopping background tracking...');
   Radar.stopTracking();
 };
 
 // Event listeners for location updates
 const listenForLocationUpdates = () => {
+  // Listen for location updates
   Radar.on('location', (result: { location: any; }) => {
     console.log('Location updated:', result.location);
   });
 
+  // Listen for events
   Radar.on('error', (err: any) => {
     console.error('Location error:', err);
   });
 };
 
-// Example usage of Radar SDK
-const setupRadarTracking = async () => {
-  const publishableKey = process.env.EXPO_PUBLIC_RADAR_PUBLISHABLE_KEY as string;  // Replace with your actual Radar publishable key
-  const { data: { user } } = await supabase.auth.getUser()
-  if (!user) throw new Error('User not found');
-  const userId = user?.id as string;
-  const userData = await getUserData(userId);
-
-  // Initialize Radar SDK, set user ID, 
-  initializeRadar(publishableKey);
-  // Radar.setUserId(userId);
-  // Radar.setMetadata(userData)
-  // Radar.setDescription(`${userData.username} - ${userData.email} - ${userData.phone}`);
-
-  console.log('Radar initialized with publishable key:', publishableKey);
-  // console.log('Radar initialized with user ID:', userId);
-  // console.log('User metadata:', userData);
-  // console.log('User description:', `${userData.username} - ${userData.email} - ${userData.phone}`);
-
-  // Request location permissions
-  await requestLocationPermissions();
-
-  // Start location tracking
-  trackLocationOnce(); // Track location once in foreground
-  console.log('Location tracked once in foreground');
-  startBackgroundTracking('RESPONSIVE'); // Start background tracking with responsive preset
-  console.log('Background tracking started: RESPONSIVE');
-  listenForLocationUpdates(); // Listen for location updates
-};
-
 export {
+  cancelTrip,
   initializeRadar,
-  setupRadarTracking,
-  requestLocationPermissions,
   trackLocationOnce,
-  startBackgroundTracking,
   stopBackgroundTracking,
+  startBackgroundTracking,
   listenForLocationUpdates,
+  requestLocationPermissions,
 };
