138b56ed4fb5ac21cf8b0c15fbce06a1550f757f
alifmaulidanar <alifmaulidanr@gmail.com>
2024-12-29 00:24:47 +0700

    Improved data handling in radar to also store and update data to Supabase

--- src/utils/radar.ts
+++ src/utils/radar.ts
@@ -1,6 +1,8 @@
 import 'react-native-get-random-values';
 import { v7 as uuid } from 'uuid';
 import Radar from 'react-native-radar';
+import { updateTicket } from '../api/tickets';
+import { createTrip, getTrip, getTripIdByTicketId, updateTrip } from '../api/trip';
 
 /**
  * Initialize Radar SDK
@@ -31,10 +33,10 @@ const requestLocationPermissions = async () => {
 const trackLocationOnce = async () => {
   try {
     const result = await Radar.trackOnce();
-    console.log('TrackOnce Result:');
-    console.log('Location:', result.location);
-    console.log('Events:', result.events);
-    console.log('User Metadata:', result.user);
+    // console.log('TrackOnce Result:');
+    // console.log('Location:', result.location);
+    // console.log('Events:', result.events);
+    // console.log('User Metadata:', result.user);
   } catch (err) {
     console.error('Error in TrackOnce:', err);
   }
@@ -69,7 +71,7 @@ const startBackgroundTracking = async (user_id: string, username: string, ticket
       username: username
     }
   };
-  console.log('Default trip options:', defaultTripOptions);
+  // console.log('Default trip options:', defaultTripOptions);
   const defaultTrackingOptions: any = {
     desiredStoppedUpdateInterval: 30,
     fastestStoppedUpdateInterval: 30,
@@ -92,7 +94,7 @@ const startBackgroundTracking = async (user_id: string, username: string, ticket
     foregroundServiceEnabled: true,
     beacons: false,
   };
-  console.log('Default tracking options:', defaultTrackingOptions);
+  // console.log('Default tracking options:', defaultTrackingOptions);
 
   try {
     // Start trip tracking (with default trip options and tracking options)
@@ -103,11 +105,34 @@ const startBackgroundTracking = async (user_id: string, username: string, ticket
     });
     console.log('startTrip()');
 
+    trackLocationOnce();
+    console.log('trackLocationOnce()');
+
     // Start trip tracking (with default trip options and preset "CONTINUOUS" tracking options)
     // const result = await Radar.startTrip({ tripOptions: defaultTripOptions });
     // Start continuous tracking for background updates with CONTINUOUS preset
     // Radar.startTrackingContinuous();
     // console.log('Continuous tracking started for background updates.');
+
+    // Get trip's information from Radar
+    const tripData = await getTrip(tripExternalId);
+
+    // Create a new Trip in Supabase
+    await createTrip(
+      tripData.radar_id,
+      tripData.external_id,
+      tripData.user_id,
+      tripData.geofence_id,
+      tripData.geofence_tag,
+      tripData.mode,
+      tripData.status,
+      tripData.duration,
+      tripData.live,
+      tripData.approaching_threshold
+    );
+
+    // Update ticket status to "on_progress" in Supabase
+    await updateTicket(ticket_id, tripExternalId, 'on_progress');
 
     console.log('Trip started successfully:', result);
   } catch (err) {
@@ -124,9 +149,23 @@ const startBackgroundTracking = async (user_id: string, username: string, ticket
 /**
  * Stop trip and background tracking
  */
-const stopBackgroundTracking = async () => {
+const stopBackgroundTracking = async (ticket_id: string) => {
   try {
+    // Get tripId from Supabase by ticketId
+    const trip_id = await getTripIdByTicketId(ticket_id);
+
+    // Get trip's information from Radar
+    const tripData = await getTrip(trip_id);
+
+    // Stop trip tracking and complete trip by Radar SDK
     const result = await Radar.completeTrip();
+
+    // Update ticket status to "completed" and duration in Supabase
+    await updateTrip(trip_id, 'completed', tripData.duration);
+
+    // Update ticket status to "completed" in Supabase
+    await updateTicket(ticket_id, trip_id, 'completed');
+
     console.log('Trip completed successfully:', result);
   } catch (err) {
     console.error('Error completing trip:', err);
@@ -139,9 +178,23 @@ const stopBackgroundTracking = async () => {
 /**
  * Cancel trip and stop background tracking
  */
-const cancelTrip = async () => {
+const cancelTrip = async (ticket_id: string) => {
   try {
+    // Get tripId from Supabase by ticketId
+    const trip_id = await getTripIdByTicketId(ticket_id);
+
+    // Get trip's information from Radar
+    const tripData = await getTrip(trip_id);
+
+    // Cancel trip by Radar SDK
     const result = await Radar.cancelTrip();
+
+    // Update ticket status to "canceled" and duration in Supabase
+    await updateTrip(trip_id, 'canceled', tripData.duration);
+
+    // Update ticket status to "canceled" in Supabase
+    await updateTicket(ticket_id, trip_id, 'canceled');
+
     console.log('Trip canceled successfully:', result);
   } catch (err) {
     console.error('Error canceling trip:', err);
@@ -171,35 +224,35 @@ const listenForLocationUpdates = () => {
 /**
  * Start periodic trip updates
  */
-const startPeriodicTripUpdates = (tripId: string) => {
-  console.log(`Starting periodic trip updates for tripId: ${tripId}`);
+// const startPeriodicTripUpdates = (tripId: string) => {
+//   console.log(`Starting periodic trip updates for tripId: ${tripId}`);
 
-  // Update trip every 30 seconds
-  const updateInterval = 30000;
+//   // Update trip every 30 seconds
+//   const updateInterval = 30000;
 
-  const intervalId = setInterval(async () => {
-    try {
-      const result = await Radar.updateTrip({
-        status: "unknown",
-        options: {
-          externalId: tripId, // ID trip
-          metadata: {
-            latitude: "37.773972",
-            longitude: "-122.431297",
-          },
-          destinationGeofenceTag: 'delivery-point',
-          destinationGeofenceExternalId: 'dest-123',
-        },
-      });
-      console.log('Trip updated:', result);
-    } catch (err) {
-      console.error('Error updating trip:', err);
-    }
-  }, updateInterval);
+//   const intervalId = setInterval(async () => {
+//     try {
+//       const result = await Radar.updateTrip({
+//         status: "unknown",
+//         options: {
+//           externalId: tripId, // ID trip
+//           metadata: {
+//             latitude: "37.773972",
+//             longitude: "-122.431297",
+//           },
+//           destinationGeofenceTag: 'delivery-point',
+//           destinationGeofenceExternalId: 'dest-123',
+//         },
+//       });
+//       console.log('Trip updated:', result);
+//     } catch (err) {
+//       console.error('Error updating trip:', err);
+//     }
+//   }, updateInterval);
 
-  // Store intervalId in state
-  return intervalId;
-};
+//   // Store intervalId in state
+//   return intervalId;
+// };
 
 export {
   initializeRadar,
