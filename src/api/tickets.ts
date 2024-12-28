21817020d6fb86cfd24ebdefe4e9d49088517d90
alifmaulidanar <alifmaulidanr@gmail.com>
2024-12-29 00:23:14 +0700

    Added new functionality to update a ticket

--- src/api/tickets.ts
+++ src/api/tickets.ts
@@ -23,3 +23,25 @@ export const getTickets = async (user_id: string): Promise<Ticket[]> => {
     throw error;
   }
 }
+
+// Update tripID and ticket status in table tickets in Supabase by ticket ID by backend API
+export const updateTicket = async (ticket_id: string, trip_id: string, status: string): Promise<void> => {
+  try {
+    const response = await fetch(`${process.env.EXPO_PUBLIC_API_BASE_URL as string}/ticket/status`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ticket_id, trip_id, status }),
+    });
+
+    if (!response.ok) {
+      const data = await response.json();
+      console.error("Error updating ticket:", data.message || "Unknown error");
+      throw new Error(data.message || "Unknown error");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+}
