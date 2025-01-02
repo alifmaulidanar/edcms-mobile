727e4ac9c6d725b7eda1067f45d384a9cc39e0d1
alifmaulidanar <alifmaulidanr@gmail.com>
2025-01-02 23:56:37 +0700

    Commented all notification functions

--- src/utils/notification.ts
+++ src/utils/notification.ts
@@ -1,22 +1,22 @@
-import * as Notifications from 'expo-notifications';
+// import * as Notifications from 'expo-notifications';
 
-// Setup notification channel
-export const setupNotificationChannel = async (): Promise<void> => {
-  await Notifications.setNotificationChannelAsync('tracking', {
-    name: 'Tracking',
-    importance: Notifications.AndroidImportance.HIGH,
-    vibrationPattern: [0, 250, 250, 250],
-    lightColor: '#FF231F7C',
-  })
-}
+// // Setup notification channel
+// export const setupNotificationChannel = async (): Promise<void> => {
+//   await Notifications.setNotificationChannelAsync('tracking', {
+//     name: 'Tracking',
+//     importance: Notifications.AndroidImportance.HIGH,
+//     vibrationPattern: [0, 250, 250, 250],
+//     lightColor: '#FF231F7C',
+//   })
+// }
 
-// Send tracking notification
-export const sendTrackingNotification = async (): Promise<void> => {
-  await Notifications.scheduleNotificationAsync({
-    content: {
-      title: 'Tracking Active',
-      body: 'Your location is being updated in the background!',
-    },
-    trigger: null,
-  })
-}
+// // Send tracking notification
+// export const sendTrackingNotification = async (): Promise<void> => {
+//   await Notifications.scheduleNotificationAsync({
+//     content: {
+//       title: 'Tracking Active',
+//       body: 'Your location is being updated in the background!',
+//     },
+//     trigger: null,
+//   })
+// }
